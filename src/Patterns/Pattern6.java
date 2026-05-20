64d37c2c33acfe1af2d5ed63e488d6305de4aa43
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-20 19:58:41 +0530

    inverted right-angled numbered triangle with numbers

--- src/Patterns/Pattern6.java
+++ src/Patterns/Pattern6.java
@@ -4,7 +4,7 @@ public class Pattern6 {
     static void main() {
         int n = 5;
         for(int i = 1; i <= n; i++){
-            for(int j = i; j <= n; j++){
+            for(int j = 1; j <= n-i+1; j++){
                 System.out.print(j);
             }
             System.out.println();
