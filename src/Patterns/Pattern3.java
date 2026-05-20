fd46d2609f59162030e60b9a359ff4a55bcf17aa
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-20 19:29:37 +0530

    right-angled numbered triangle

---
+++ src/Patterns/Pattern3.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class Pattern3 {
+    static void main() {
+        int n = 5;
+        for(int i = 0; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
