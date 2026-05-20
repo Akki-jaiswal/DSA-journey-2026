07ab2f385db93f768d97bb8034478a8851894d6c
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-20 19:46:40 +0530

    flipped right-angled numbered triangle

---
+++ src/Patterns/Pattern5.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Pattern5 {
+    static void main() {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for (int j = i; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
