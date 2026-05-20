a1b2d9e58f4331df71e99c58cdb00d720f76c856
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-21 04:49:45 +0530

    Half-pyramid

--- src/Patterns/Pattern10.java
+++ src/Patterns/Pattern10.java
@@ -1,4 +1,21 @@
 package Patterns;
 
 public class Pattern10 {
+    static void main() {
+        int N = 5;
+        for(int i = 0; i <= N; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = N-1; i > 0; i-- ){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
