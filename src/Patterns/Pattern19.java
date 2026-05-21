3fa6b9a9347b90c5d36f8253e35008a68e6e77f8
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-21 11:13:36 +0530

    hourglass-shape of stars

---
+++ src/Patterns/Pattern19.java
@@ -0,0 +1,33 @@
+package Patterns;
+
+public class Pattern19 {
+    static void main() {
+        int n = 5;
+        //first part
+        for(int i=0; i<n; i++){
+            for(int j=0; j<(n-i);j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<2*i+1; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<(n-i);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //second part
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
