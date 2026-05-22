53f6d20a9cab5dc036a887dcdae97d6adb6c9be5
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-22 19:20:44 +0530

    Basics of Recursion

--- src/BasicRecursion/Print_1toN.java
+++ src/BasicRecursion/Print_1toN.java
@@ -1,16 +1,16 @@
 package BasicRecursion;
 
 public class Print_1toN {
-    public void printNumbers(int current, int n) {
+    public void printNumbers(int current) {
         // Base case: if current exceeds n, stop recursion
-        if (current > n)
+        if (current < 1)
             return;
 
         // Print current number --> forward rec.
-        //System.out.print(current + " ");
+//        System.out.print(current + " ");
 
         // Recursive call with next number
-        printNumbers(current + 1, n);
+        printNumbers(current - 1);
 
         //Print current number --> BACKTRACKING.
         System.out.print(current+" ");
@@ -20,7 +20,7 @@ public class Print_1toN {
         Print_1toN sol = new Print_1toN();
         int n = 10;
 
-        sol.printNumbers(1, n);
+        sol.printNumbers(n);
         System.out.println();
     }
 }
