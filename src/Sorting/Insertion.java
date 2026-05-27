b61c0b1b1318346b8123b22e6bb6e5cfea7aa638
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-27 11:09:04 +0530

    Performing insertion sort algorithm...

---
+++ src/Sorting/Insertion.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class Insertion {
+    static void main(String[] args) {
+        int[] arr = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+
+        for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && arr[j-1]>arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
