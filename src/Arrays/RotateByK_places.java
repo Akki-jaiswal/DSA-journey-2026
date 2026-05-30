230cb4d8166c6bf553b328f507de07ca1dd06033
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-05-30 14:10:08 +0530

    Rotate left by 1 place

---
+++ src/Arrays/RotateByK_places.java
@@ -0,0 +1,4 @@
+package Arrays;
+
+public class RotateByK_places {
+}
--- src/Arrays/RotateLeft.java
+++ src/Arrays/RotateLeft.java
@@ -4,17 +4,17 @@ public class RotateLeft {
     static void main(String[] args) {
         int n=5;
         int[] arr = {1,2,3,4,5};
-        int[] temp = new int[n];
-        for(int i=0; i<n; i++){
+        int temp = arr[0];
+        for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println();
-        for(int i=1; i<n; i++){
-            temp[i-1] = arr[i];
+        for(int i=1; i<arr.length; i++){
+            arr[i-1] = arr[i];
         }
-        temp[n-1] = arr[0];
-        for(int i=0; i<n; i++){
-            System.out.print(temp[i]+" ");
+        arr[arr.length-1] = temp;
+        for(int num:arr){
+            System.out.print(num+" ");
         }
 
     }
