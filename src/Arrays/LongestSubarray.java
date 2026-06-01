b59a25c6fe14ffa05871147ac016775c61d5c008
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-06-01 21:49:33 +0530

    Finding longest subarray..

---
+++ src/Arrays/LongestSubarray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class LongestSubarray{
+    public int longestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        int maxLen = 0;
+        int left = 0, right = 0;
+        int sum = nums[0];
+        while (right < n) {
+            while (left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+            if (right < n) {
+                sum += nums[right];
+            }
+        }
+        return maxLen;
+    }
+}
+void main(String[] args){
+    int[] nums = {10,5,2,7,1,9};
+    int k = 15;
+    LongestSubarray sol = new LongestSubarray();
+    System.out.println(sol.longestSubarray(nums,k));
+}
