b189a58f40dac6c2d728ff76ac91479f40bc3ac7
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-06-04 06:25:49 +0530

    max sum of subarray

--- src/Arrays/Medium/MaxSubarraySum.java
+++ src/Arrays/Medium/MaxSubarraySum.java
@@ -5,16 +5,29 @@ public class MaxSubarraySum {
     int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
     long maxi = Long.MIN_VALUE;
     long sum = 0;
+    int start = 0;
+    int ansStart=-1, ansEnd=-1;
     for(int i=0; i<arr.length; i++) {
+        if(sum==0){
+            start=i;
+        }
+        //Add current element to sum
         sum+=arr[i];
         if(sum>maxi){
             maxi = sum;
+            ansStart=start;
+            ansEnd=i;
         }
         if(sum<0){
             sum=0;
         }
 
     }
-        System.out.println(maxi);
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("]");
+        System.out.println("Sum is "+maxi);
     }
 }
