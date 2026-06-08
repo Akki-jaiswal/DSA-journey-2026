83bb71369462a7d10ae10e143766393deb25ec67
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-06-08 19:48:41 +0530

    Longest subarray of sum K..

---
+++ src/Arrays/Medium/PascalTriangle.java
@@ -0,0 +1,15 @@
+package Arrays.Medium;
+
+public class PascalTriangle {
+    static void main(String[] args) {
+        int r = 5, c = 3;
+        int n = r-1;
+        int k = c-1;
+        long result = 1;
+        for(int i=0; i<k; i++){
+            result*=(n-1);
+            result/=(i+1);
+        }
+        System.out.println(result);
+    }
+}
