ecebb35b85fee64f3ba8c8dbf860fe5f844f97c9
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-06-16 09:03:53 +0530

    Min element in rotated sorted array..

---
+++ src/BinarySearch/IDArrays/MinRotatedSorted.java
@@ -0,0 +1,18 @@
+package BinarySearch.IDArrays;
+
+public class MinRotatedSorted {
+    static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,-1,2};
+        int low=0,high=nums.length-1;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if (nums[mid]>nums[high]){
+                low = mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        System.out.println(nums[low]);
+    }
+}
