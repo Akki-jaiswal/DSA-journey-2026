ca083f6748e65338f8f79e14f3df32a43f5e5b55
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-07-24 09:23:23 +0530

    converted postfix to infix..

---
+++ src/StackQueues/Conversion/postTOin.java
@@ -0,0 +1,26 @@
+package StackQueues.Conversion;
+
+import java.util.Stack;
+
+public class postTOin {
+    public String postfixToInfix(String postfix){
+        Stack<String> s = new Stack<>();
+        int n = postfix.length();
+        for (int i=0; i<n; i++){
+            char c=postfix.charAt(i);
+            if (Character.isLetterOrDigit(c)){
+                s.push(String.valueOf(c));
+            }else{
+                String op2 = s.pop();
+                String op1 = s.pop();
+                s.push("("+op1+c+op2+")");
+            }
+        }
+        return s.peek();
+    }
+    static void main(String[] args) {
+        postTOin converter = new postTOin();
+        String postfix = "AB*C+";
+        System.out.println("Infix Expression: " + converter.postfixToInfix(postfix));
+    }
+}
