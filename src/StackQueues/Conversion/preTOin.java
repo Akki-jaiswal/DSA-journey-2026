6cb2f552da7bfd481ef857c7da74f474db6f336c
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-07-24 19:54:53 +0530

    converted prefix to infix..

---
+++ src/StackQueues/Conversion/preTOin.java
@@ -0,0 +1,28 @@
+package StackQueues.Conversion;
+
+import java.util.Stack;
+
+public class preTOin {
+    public String prefixToInfix(String prefix){
+        Stack<String> s = new Stack<>();
+        int n = prefix.length();
+        for (int i=n-1; i>=0; i--){
+            char c=prefix.charAt(i);
+            if (Character.isLetterOrDigit(c)){
+                s.push(String.valueOf(c));
+            }else{
+                String op1 = s.pop();
+                String op2 = s.pop();
+                s.push("(" + op1 + c + op2 + ")");
+            }
+        }
+        return s.peek();
+    }
+    static void main(String[] args) {
+        preTOin converter = new preTOin();
+        String prefix = "*-A/BC-/AKL";
+        System.out.println("Prefix Expression: " + prefix);
+
+        System.out.println("Infix Expression: " + converter.prefixToInfix(prefix));
+    }
+}
