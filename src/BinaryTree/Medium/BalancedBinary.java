6ddcee503d2956af4cf11a50c6cfca8e238e0d40
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-08-24 19:09:02 +0530

    left-right>1 not balanced..

--- src/BinaryTree/Medium/BalancedBinary.java
+++ src/BinaryTree/Medium/BalancedBinary.java
@@ -9,7 +9,7 @@ public class BalancedBinary {
         int leftHeight = dfsHeight(root.left);
         if (leftHeight==-1)
             return -1;
-        int rightHeight = dfsheight(root.right);
+        int rightHeight = dfsHeight(root.right);
         if (rightHeight==-1)
             return -1;
 
