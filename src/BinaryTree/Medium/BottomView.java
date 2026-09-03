58f3c47cc7253061e8b30c5c54d30d4f86ab0cbf
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-09-03 06:40:34 +0530

    Traversing tree from bottom..

---
+++ src/BinaryTree/Medium/BottomView.java
@@ -0,0 +1,53 @@
+package BinaryTree.Medium;
+
+import java.util.*;
+
+public class BottomView {
+    public List<Integer> bottomView(Node root){
+        List<Integer> ans = new ArrayList<>();
+        if (root==null)
+            return ans;
+        Map<Integer, Integer> mpp = new TreeMap<>();
+        Queue<TopView.Pair> q = new LinkedList<>();
+        q.add(new TopView.Pair(root, 0));
+        while (!q.isEmpty()){
+            TopView.Pair pair = q.poll();
+            Node node = pair.node;
+            int line = pair.line;
+            mpp.put(line, node.data);
+            if (node.left!=null){
+                q.add(new TopView.Pair(node.left, line-1));
+            }
+            if (node.right!=null){
+                q.add(new TopView.Pair(node.right, line+1));
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry:mpp.entrySet()){
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+    void main(String[] args) {
+        // Creating a sample binary tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(10);
+        root.left.left.right = new Node(5);
+        root.left.left.right.right = new Node(6);
+        root.right = new Node(3);
+        root.right.right = new Node(10);
+        root.right.left = new Node(9);
+
+        BottomView solution = new BottomView();
+
+        // Get the Bottom View traversal
+        List<Integer> bottomView = solution.bottomView(root);
+
+        // Print the result
+        System.out.println("Bottom View Traversal: ");
+        for(int node : bottomView) {
+            System.out.print(node + " ");
+        }
+    }
+}
