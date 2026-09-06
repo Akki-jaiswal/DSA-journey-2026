d89651575197a01519bc4598b05e3d59f2b67ffb
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-09-06 06:56:52 +0530

    finding the least node value in the binary tree

---
+++ src/BinaryTree/Hard/LCommonAncestor.java
@@ -0,0 +1,42 @@
+package BinaryTree.Hard;
+
+public class LCommonAncestor {
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            data = val;
+            left = null;
+            right = null;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if (root==null || root==p || root==q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left==null) return right;
+        if (right==null) return left;
+        return root;
+    }
+    static void main(String[] args) {
+        // Construct a sample binary tree
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        LCommonAncestor solution = new LCommonAncestor();
+        TreeNode p = root.left;       // Node with value 5
+        TreeNode q = root.right;      // Node with value 1
+
+        TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+        System.out.println("Lowest Common Ancestor: " + lca.data);
+    }
+
+}
