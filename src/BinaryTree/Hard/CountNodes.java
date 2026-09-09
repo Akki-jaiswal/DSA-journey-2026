2406413ad9a978155ac80771f439718ee2b9a953
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-09-09 06:42:31 +0530

    If lh=rh use 1<<lh-1 else return 1+leftN+rightN

---
+++ src/BinaryTree/Hard/CountNodes.java
@@ -0,0 +1,46 @@
+package BinaryTree.Hard;
+
+public class CountNodes {
+    public int countNodes(TreeNode root){
+        if (root==null) return 0;
+        int lh = findHeightLeft(root);
+        int rh = findHeightRight(root);
+        if (lh==rh) return (1<<lh)-1;
+        return 1+countNodes(root.left)+countNodes(root.right);
+    }
+    public int  findHeightLeft(TreeNode node){
+        int height=0;
+        while (node!=null){
+            height++;
+            node=node.left;
+        }
+        return height;
+    }
+    public int findHeightRight(TreeNode node){
+        int height=0;
+        while (node!=null){
+            height++;
+            node=node.right;
+        }
+        return height;
+    }
+    static void main(String[] args) {
+        // Create binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+
+        // Create solution object
+        CountNodes sol = new CountNodes();
+
+        // Count total nodes
+        int totalNodes = sol.countNodes(root);
+
+        // Print result
+        System.out.println("Total number of nodes in the Complete Binary Tree: " + totalNodes);
+    }
+}
+
