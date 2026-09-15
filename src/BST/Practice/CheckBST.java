2b87f74678ab7da9a67c70e34241e8406f373a65
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-09-15 06:29:29 +0530

    If both child and its ancestors are in the range..

---
+++ src/BST/Practice/CheckBST.java
@@ -0,0 +1,14 @@
+package BST.Practice;
+
+import java.security.PublicKey;
+
+public class CheckBST {
+    public boolean isValid(TreeNode root){
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private boolean validate(TreeNode node, long min, long max){
+        if (node==null) return true;
+        if (node.val<=min || node.val>=max) return false;
+        return validate(node.left, min, node.val)&&validate(node.right, node.val, max);
+    }
+}
